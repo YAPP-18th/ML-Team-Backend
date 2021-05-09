4c444fe1b900574ec1c27e5ca76f14f8ee1cf594
junkyu.lim <junkyu.lim.dev@gmail.com>
2021-05-09 14:29:26 +0900

    추가 : user crud 및 필요 로직 정의
    필요로직 -> findByEmail

---
+++ app/crud/user.py
@@ -0,0 +1,16 @@
+from typing import Optional, List
+
+from sqlalchemy.orm import Session
+
+from app.crud.base import CRUDBase
+from app.models import User
+from app.schemas import UserCreate, UserUpdate
+
+
+class CRUDUser(CRUDBase[User, UserCreate, UserUpdate]):
+    # Declare model specific CRUD operation methods.
+    def get_one_by_email(self, db: Session, email: str):
+        return db.query(User).filter(User.social_id == email).first()
+
+
+user = CRUDUser(User)
