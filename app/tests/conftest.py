5f23df071776cc2f17fd484bf4ff57e35f5faa01
2_taehyun <63915557+0417taehyun@users.noreply.github.com>
2021-05-09 19:04:42 +0900

    WIP

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -1,21 +1,12 @@
 from typing               import Dict
+
 from fastapi.testclient   import TestClient
 
 from app.database.session import SessionLocal
 from app.database.base    import Base
 from app.api.deps         import get_db
 from app.main             import app
-<<<<<<< HEAD
 
-Base.metadata
-
-def overried_get_db():
-    try:
-        db = SessionLocal()
-        yield db
-    finally:
-        db.close()
-=======
 
 
 Base.metadata
@@ -27,38 +18,14 @@ def overried_get_db():
     finally:
         db.close()
 
-<<<<<<< HEAD
-=======
->>>>>>> 1e939ba... 추가: Study Room
 
->>>>>>> bdaf617... 수정: Conflict 해결 및 사용자 라우터 추가
 def client():
     with TestClient(app) as client:
         yield client
 
-<<<<<<< HEAD
-def client():
-    with TestClient(app) as client:
-        yield client
-=======
->>>>>>> 1e939ba... 추가: Study Room
 
 app.dependency_overrides[get_db] = overried_get_db
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-app.dependency_overrides[get_db] = overried_get_db
-
-<<<<<<< HEAD
-@pytest.fixture(scope="module")
-def random_product() -> Dict[str, str]:
-    return {
-        "id": 1,
-        "name": "Test Product",
-        "price": 80,
-    }
->>>>>>> bdaf617... 수정: Conflict 해결 및 사용자 라우터 추가
 
 
 def test_user() -> Dict[str, str]:
@@ -70,10 +37,3 @@ def test_user() -> Dict[str, str]:
     }
 
 client = TestClient(app)
-<<<<<<< HEAD
-=======
->>>>>>> 15048eb... 수정: 전체적인 폴더 구조 리팩토링
-=======
-client = TestClient(app)
->>>>>>> 1e939ba... 추가: Study Room
->>>>>>> bdaf617... 수정: Conflict 해결 및 사용자 라우터 추가
