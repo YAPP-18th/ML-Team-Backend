28af917ce69ecd9c3b0bf9c5a59d05a451d51500
2_taehyun <63915557+0417taehyun@users.noreply.github.com>
2021-05-09 18:56:46 +0900

    기타: Conflict 해결

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,8 +1,2 @@
-<<<<<<< HEAD
 from app.models.users       import User
 from app.models.study_rooms import StudyRooms
-=======
-from app.models.product import Product
-from app.models.user.user import User
-from app.models.study_rooms import StudyRooms
->>>>>>> 1e939ba... 추가: Study Room
--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from app.schemas.user        import UserBase, UserCreate, UserUpdate, UserResponse
 from app.schemas.study_rooms import (
                                 StudyRoomsBase,
@@ -7,8 +6,3 @@ from app.schemas.study_rooms import (
                                 StudyRoomResponse
                             )
 
-=======
-from app.schemas.message import Message
-from app.schemas.product import ProductBase, ProductCreate, ProductUpdate, ProductResponse
-from app.schemas.study_rooms import StudyRoomsBase, StudyRoomsCreate, StudyRoomsUpdate, StudyRoomResponse
->>>>>>> 1e939ba... 추가: Study Room
