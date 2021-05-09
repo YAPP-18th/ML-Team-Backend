ddc3ff139025ce093b75a1ab0603be4fc377c09d
2_taehyun <63915557+0417taehyun@users.noreply.github.com>
2021-05-09 18:54:24 +0900

    수정: Conflict 해결 및 사용자 라우터 추가

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import uvicorn
 
+<<<<<<< HEAD
 from fastapi  import FastAPI
 
 <<<<<<< HEAD
@@ -26,3 +27,19 @@ server.include_router(api_router, prefix=common_settings.COMMON_API)
 if __name__ == "__main__":
     uvicorn.run('app.main:server', host="0.0.0.0", port=8000, reload=True)
 >>>>>>> 15048eb... 수정: 전체적인 폴더 구조 리팩토링
+=======
+from fastapi                   import FastAPI
+
+from app.core   import settings
+from app.api.v1 import api_router
+
+server = FastAPI(title=settings.PROJECT_NAME)
+
+
+
+server.include_router(api_router, prefix=settings.API_V1_STR)
+
+
+if __name__ == "__main__":
+    uvicorn.run('app.main:server', host="0.0.0.0", port=8000, reload=True)
+>>>>>>> 1e939ba... 추가: Study Room
--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from app.schemas.user        import UserBase, UserCreate, UserUpdate, UserResponse
 from app.schemas.study_rooms import (
                                 StudyRoomsBase,
@@ -6,3 +7,8 @@ from app.schemas.study_rooms import (
                                 StudyRoomResponse
                             )
 
+=======
+from app.schemas.message import Message
+from app.schemas.product import ProductBase, ProductCreate, ProductUpdate, ProductResponse
+from app.schemas.study_rooms import StudyRoomsBase, StudyRoomsCreate, StudyRoomsUpdate, StudyRoomResponse
+>>>>>>> 1e939ba... 추가: Study Room
