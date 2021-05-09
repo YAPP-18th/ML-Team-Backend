d35aaeb82f568678e1d80ea779bba4c147b87534
2_taehyun <63915557+0417taehyun@users.noreply.github.com>
2021-05-09 19:10:41 +0900

    기타: Conflict 해결 및 불필요한 파일 제거

--- app/core/config.py
+++ app/core/config.py
@@ -3,12 +3,8 @@ import secrets
 from pydantic import BaseSettings
 
 
-
 class CommonSettings(BaseSettings):
     COMMON_API: str = '/api'
-    PROJECT_NAME: str = "studeep"
-    SECRET_KEY: str = secrets.token_urlsafe(32)
-    SQLALCHEMY_DATABASE_URI: str = "postgresql://localhost:5432/studeep"
     PROJECT_NAME: str = "studeep"
     SECRET_KEY: str = secrets.token_urlsafe(32)
     SQLALCHEMY_DATABASE_URI: str = "postgresql://localhost:5432/studeep"
--- app/main.py
+++ app/main.py
@@ -12,5 +12,3 @@ server.include_router(api_router, prefix=common_settings.COMMON_API)
 
 if __name__ == "__main__":
     uvicorn.run('app.main:server', host="0.0.0.0", port=8000, reload=True)
-
-
