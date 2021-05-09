0924fd19889d1890c000f263379906d3107f4fc3
2_taehyun <63915557+0417taehyun@users.noreply.github.com>
2021-05-09 19:00:37 +0900

    기타: 오타 수정

--- app/api/v1/__init__.py
+++
@@ -1,6 +0,0 @@
-from fastapi    import APIRouter
-
-from app.api.v1 import study_rooms
-
-api_router = APIRouter()
-api_router.include_router(study_rooms.router, prefix="/study-rooms", tags=["study_rooms"])
