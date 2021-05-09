6e6f61e480b3bc5fe8d339414cc29c5f695ede09
junkyu.lim <junkyu.lim.dev@gmail.com>
2021-05-09 14:34:02 +0900

    추가 : 개발사항 의존성 추가

--- app/database/base.py
+++ app/database/base.py
@@ -1,2 +1,3 @@
 from app.database.base_class import Base
 from app.models.product import Product
+from app.models.user.user import User
--- app/main.py
+++ app/main.py
@@ -3,11 +3,13 @@ import uvicorn
 from fastapi import FastAPI
 
 from app.api.v1 import api_router
+from app.api import user_router
 from app.core import settings
 
 app = FastAPI(title=settings.PROJECT_NAME)
 
 app.include_router(api_router, prefix=settings.API_V1_STR)
+app.include_router(user_router, prefix=settings.API_USER)
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8001)
