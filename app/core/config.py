1b6be46972f4add86b09fb137c018cbed4027b31
junkyu.lim <junkyu.lim.dev@gmail.com>
2021-05-09 14:33:49 +0900

    추가 : user api / token expire time config 추가

--- app/core/config.py
+++ app/core/config.py
@@ -5,10 +5,12 @@ from pydantic import BaseSettings
 
 class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
+    API_USER: str = "/api/user"
     PROJECT_NAME: str = "FastAPI"
     SECRET_KEY: str = secrets.token_urlsafe(32)
     SQLALCHEMY_DATABASE_URI: str = "postgresql://localhost:5432/fastapi_db"
     ALGORITHM = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
     class Config:
         env_file = ".env"
