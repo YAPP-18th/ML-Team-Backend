44f105b2075a25b1e73e4fbc204351f504b4c49e
junkyu.lim <junkyu.lim.dev@gmail.com>
2021-05-15 23:55:36 +0900

    수정 : user api 구조 및 response 형태 변경

--- app/api/users.py
+++ app/api/users.py
@@ -1,52 +1,107 @@
 import logging
+import traceback
 from datetime import timedelta
 from typing import Optional
 
-from fastapi import APIRouter, Depends, Header, HTTPException, Response
-from fastapi.logger import logger
+from fastapi import APIRouter, Depends, Header, Response
 from jose import JWTError
 from sqlalchemy.orm import Session
+from starlette import status
+from starlette.responses import JSONResponse
 
 from app import schemas, crud
 from app.api.deps import get_db
-from app.schemas import UserResponse
+from app.erros import get_detail, NoSuchElementException
+from app.schemas import SuccessResponseBase, ErrorResponseBase, NotFoundUserHandling, UnauthorizedHandler
 from app.service import auth
 from app.core import user_settings
-from app.service.auth import get_token_in_header
 
 router = APIRouter()
 
 
-@router.post("/signup", response_model=UserResponse)
+@router.post(
+    "/signup",
+    responses = {
+        200: {
+            "model": SuccessResponseBase,
+            "description": "회원가입 성공"
+        },
+        401: {
+            "model": UnauthorizedHandler,
+            "description": "JWT 토큰 인증에 실패하였을 경우"
+        },
+        500: {
+            "model": ErrorResponseBase,
+            "description": "Generic Error"
+        }
+    }
+)
 def sign_up(*, db: Session = Depends(get_db), user_in: schemas.UserCreate, authorization: Optional[str] = Header(None)):
-    jwt = get_token_in_header(authorization)
-    if auth.check_access_token_valid(jwt):
+    try:
+        email = auth.check_access_token_valid(authorization)
+        user_in.social_id = email
         user = crud.user.create(db, obj_in=user_in)
-        return user
-    else:
-        raise HTTPException(status_code=401, detail='Invalid token')
+        return JSONResponse(status_code=status.HTTP_200_OK, content={'data': user})
+    except JWTError:
+        message = traceback.format_exc()
+        detail  = get_detail(param='token', field='authorize', message=message, err='invalid token')
+        return JSONResponse(status_code=status.HTTP_401_UNAUTHORIZED, content={'detail': detail})
+    except Exception as error:
+        logging.error(traceback.format_exc())
+        return JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                            content={'detail': f'server error: {traceback.format_exc()}'})
+    finally:
+        db.close()
 
 
-@router.get("/signin", response_model=UserResponse)
+@router.get(
+    "/signin",
+    responses = {
+        200: {
+            "model": SuccessResponseBase,
+            "description": "로그인 성공"
+        },
+        401: {
+            "model": UnauthorizedHandler,
+            "description": "Google 토큰 인증에 실패하였을 경우"
+        },
+        404: {
+            "model": NotFoundUserHandling,
+            "description": "user가 존재하지 않는 경우"
+        },
+        500: {
+            "model": ErrorResponseBase,
+            "description": "Generic Error"
+        }
+    }
+)
 def sign_in(*, db: Session = Depends(get_db),
             response: Response,
             authorization: Optional[str] = Header(None)
             ):
     try:
-        email = auth.auth_google_token(get_token_in_header(authorization))
+        # raise JWT Error
+        email = auth.auth_google_token(authorization)
+        user = crud.user.get_one_by_email(db, email)
+
+        token = auth.create_access_token({"sub": email}, timedelta(minutes=user_settings.ACCESS_TOKEN_EXPIRE_MINUTES))
+
+        if user is None:
+            return JSONResponse(status_code=status.HTTP_404_NOT_FOUND,
+                                content={'detail': "user(email: " + email + ") not exist."},
+                                headers={"Authorization": 'bearer ' + token}
+                                )
+
+        response.headers["Authorization"] = 'bearer ' + token
+
+        return JSONResponse(status_code=status.HTTP_200_OK, content={'data': user})
     except JWTError:
-        raise HTTPException(status_code=401, detail="Invalid Token")
-
-    user = crud.user.get_one_by_email(db, email)
-
-    token = auth.create_access_token({"sub": email}, timedelta(minutes=user_settings.ACCESS_TOKEN_EXPIRE_MINUTES))
-
-    if user is None:
-        raise HTTPException(status_code=404,
-                            detail="user(email: " + email + ") not exist.",
-                            headers={"Authorization": 'bearer ' + token}
-                            )
-
-    response.headers["Authorization"] = 'bearer ' + token
-
-    return user
+        message = traceback.format_exc()
+        detail = get_detail(param='token', field='authorize', message=message, err='invalid Google token')
+        return JSONResponse(status_code=status.HTTP_401_UNAUTHORIZED, content={'detail': detail})
+    except Exception as error:
+        logging.error(traceback.format_exc())
+        return JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                            content={'detail': f'server error: {traceback.format_exc()}'})
+    finally:
+        db.close()
