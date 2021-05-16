fbaaea598264c0206fad3b1d6b8c2551604e6a20
junkyu.lim <junkyu.lim.dev@gmail.com>
2021-05-16 17:32:57 +0900

    추가 : user에 goal 컬럼 추가

--- app/schemas/users/crud.py
+++ app/schemas/users/crud.py
@@ -1,7 +1,9 @@
 import enum
+import json
 from typing import Optional
 
 from pydantic import BaseModel
+from sqlalchemy.dialects.postgresql import JSON
 
 
 class UserBase(BaseModel):
@@ -9,17 +11,28 @@ class UserBase(BaseModel):
     provider: Optional[str]
     social_id: Optional[str]
     nickname: Optional[str]
+    goal: Optional[dict]
 
 
 class UserCreate(UserBase):
     provider: str
     nickname: str
+    goal: dict
 
     class Config:
         schema_extra = {
             'example': {
                 'provider': 'google',
-                'nickname': 'Studeep_User'
+                'nickname': 'Studeep_User',
+                'goal': {
+                    '월': 2,
+                    '화': 2,
+                    '수': 2,
+                    '목': 2,
+                    '금': 2,
+                    '토': 2,
+                    '일': 2
+                }
             }
         }
 
@@ -34,7 +47,16 @@ class UserUpdate(UserBase):
                 'id': 1,
                 'provider': 'google',
                 'social_id': 'example@gmail.com',
-                'nickname': 'new Nickname'
+                'nickname': 'new Nickname',
+                'goal': {
+                    '월': 2,
+                    '화': 2,
+                    '수': 2,
+                    '목': 2,
+                    '금': 2,
+                    '토': 2,
+                    '일': 2
+                }
             }
         }
 
