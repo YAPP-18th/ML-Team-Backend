bbb06fa2b1c9077f36d7a5d646dc268fb04ebc99
junkyu.lim <junkyu.lim.dev@gmail.com>
2021-05-16 17:30:16 +0900

    추가 : get_user 및 user backdoor 추가

--- app/api/users.py
+++ app/api/users.py
@@ -127,3 +127,70 @@ def sign_in(*, db: Session = Depends(get_db),
                             content={'detail': f'server error: {traceback.format_exc()}'})
     finally:
         db.close()
+
+
+@router.get(
+    "/get",
+    responses = {
+        200: {
+            "model": SuccessResponseBase,
+            "description": "정상 Response"
+        },
+        401: {
+            "model": UnauthorizedHandler,
+            "description": "JWT 토큰 인증에 실패하였을 경우"
+        },
+        403: {
+            "model": ForbiddenHandler,
+            "description": "올바른 유형의 토큰이 아닌 경우(on-board / access)"
+        },
+        404: {
+            "model": NotFoundUserHandling,
+            "description": "user가 존재하지 않는 경우"
+        },
+        500: {
+            "model": ErrorResponseBase,
+            "description": "Generic Error"
+        }
+    }
+)
+def get_user(
+        *,
+        db: Session = Depends(get_db),
+        authorization: Optional[str] = Header(None)
+):
+    try:
+        # raise JWT Error
+        email = auth.check_access_token_valid(authorization)
+        user = crud.user.get_one_by_email(db, email)
+
+        if user is None:
+            raise NoSuchElementException("user(email: " + email + ") not exist.")
+
+        return JSONResponse(status_code=status.HTTP_200_OK, content={'data': jsonable_encoder(user)})
+    except JWTError:
+        message = traceback.format_exc()
+        detail = get_detail(param='token', field='authorize', message=message, err='invalid Google token')
+        return JSONResponse(status_code=status.HTTP_401_UNAUTHORIZED, content={'detail': detail})
+    except NameError:
+        message = traceback.format_exc()
+        detail = get_detail(param='token', field='forbidden', message=message,
+                            err='This token is not access token')
+        return JSONResponse(status_code=status.HTTP_403_FORBIDDEN, content={'detail': detail})
+    except Exception as error:
+        logging.error(traceback.format_exc())
+        return JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                            content={'detail': f'server error: {traceback.format_exc()}'})
+    finally:
+        db.close()
+
+
+# todo: 서비스 시작 시, 필히 제거!
+@router.get('/test/{email}/{token_type}', response_model=SuccessResponseBase, description='토큰 발급 백도어')
+def back_door_access(email: str, token_type: str):
+    if token_type == 'onboard':
+        token = auth.create_access_token({"on_board": email}, timedelta(minutes=user_settings.ACCESS_TOKEN_EXPIRE_MINUTES))
+    else:
+        token = auth.create_access_token({"sub": email}, timedelta(minutes=user_settings.ACCESS_TOKEN_EXPIRE_MINUTES))
+
+    return JSONResponse(status_code=status.HTTP_200_OK, content={'token': token})
