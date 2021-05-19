2c7a309bb26bf9513aa785514b810db8eff7c3cf
2_taehyun <63915557+0417taehyun@users.noreply.github.com>
2021-05-19 23:25:39 +0900

    수정: 스터디룸 입장 엔드포인트 및 소켓 관련 DB 로직 CRUD로 이관

--- app/api/study_rooms.py
+++ app/api/study_rooms.py
@@ -57,7 +57,6 @@ router = APIRouter()
 )
 def get_study_room(room_id: str, db: Session = Depends(get_db)):
     try:
-        print(db)
         data = study_rooms.get(db, room_id)
         return JSONResponse(status_code=status.HTTP_200_OK, content={'data': data})
 
@@ -234,7 +233,8 @@ def create_study_room(room_info: StudyRoomsCreate, db: Session = Depends(get_db)
 
 
 @router.post(
-    '/{room_id}/join',
+    # Endpoint 이름 변경
+    '/{room_id}/join-check',
     responses = {
         200: {
             "model": SuccessResponseBase,
@@ -265,7 +265,7 @@ def create_study_room(room_info: StudyRoomsCreate, db: Session = Depends(get_db)
 )
 def join_study_room(room_id: str, room_info: StudyRoomJoin, db: Session = Depends(get_db)):
     try:
-        # Redis 통해서 현재 사용자가 특정 방에 접속 중일 경우 에러 반환 로직 필요
+        # TODO: Redis 통해서 현재 사용자가 특정 방에 접속 중일 경우 에러 반환 로직 필요
         study_rooms.join(db, room_id, room_info)
         return JSONResponse(status_code=status.HTTP_200_OK, content={'data': ''})
 
--- app/service/sockets.py
+++ app/service/sockets.py
@@ -1,49 +1,46 @@
 import socketio
 
-from uuid         import UUID
+from sqlalchemy.orm.session import Session
 
-from app.core     import socket_settinngs
-from app.models   import StudyRooms
-from app.database import SessionLocal
+from app.core               import socket_settinngs
+from app.crud               import study_rooms
+from app.database           import SessionLocal
 
 
-sio           = socketio.AsyncServer(async_mode='asgi', cors_allowed_origins='*')
-namespace_url = socket_settinngs.NAMESPACE_URL
 db            = SessionLocal()
-model         = StudyRooms
+sio           = socketio.AsyncServer(async_mode='asgi', cors_allowed_origins='*', debug=True)
+namespace_url = socket_settinngs.NAMESPACE_URL
 clients       = dict()
 
 
 @sio.event(namespace=namespace_url)
 async def connect(sid, environ, auth):
-    # Redis 사용자 정보 추가
+    # TODO: Redis 사용자 정보 추가
     clients[sid] = auth['email']
     
 
 @sio.event(namespace=namespace_url)
 async def joinRoom(sid, room_id):
-    # Redis 사용자가 이용중인 스터디룸 정보 추가
+    # TODO: Redis 사용자가 이용중인 스터디룸 정보 추가
     sio.enter_room(sid=sid, room=room_id, namespace=namespace_url)
     await sio.emit('response', {'message': 'room joined'}, room=room_id, namespace=namespace_url)
 
 
 @sio.event(namespace=namespace_url)
-async def leaveRoom(sid, room_id):
-    sio.leave_room(sid=sid, room=room_id, namespace=namespace_url)
-    study_room = db.query(model).filter(model.id == UUID(room_id)).update(
-        {'current_join_counts': model.current_join_counts - 1}
-    )
-    if study_room:
+async def leaveRoom(sid, room_id, db: Session = db):
+    try:
+        # TODO: 축적된 학습 데이터 Redis > PostgreSQL 이동 필요
+        message = study_rooms.leave(db, room_id=room_id)
+        await sio.leave_room(sid=sid, room=room_id, namespace=namespace_url)
         clients.pop(sid)
-        db.commit()
-        db.close()
-        await sio.emit('response', {'message': 'room leaved'}, room=room_id, namespace=namespace_url)
-    else:
-        await sio.emit('response', {'message': 'database error'}, room=room_id, namespace=namespace_url)
+        await sio.emit('response', {'message': message}, room=room_id, namespace=namespace_url)
+        
+    except Exception as error:
+        await sio.emit('response', {'message': error}, room=room_id, namespace=namespace_url)
 
 
 @sio.event(namespace=namespace_url)
 async def status(sid, data):
-    # Redis 사용자의 학습 상태 정보 추가
+    # TODO: Redis 사용자의 학습 상태 정보 추가
     # await sio.emit('response', {'message': 'success'}, room=room_id, namespace=namespace_url)
     pass
