c772c9fe77d9013bb0c844d780916e95d84f6944
2_taehyun <63915557+0417taehyun@users.noreply.github.com>
2021-05-21 00:07:07 +0900

    수정: 소켓 통신 연결 수정

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,7 +1,8 @@
-from fastapi  import APIRouter
+from fastapi     import APIRouter, Depends
 
-from app.api  import users, study_rooms
-from app.core import study_rooms_settings, user_settings
+from app.api     import users, study_rooms
+from app.core    import study_rooms_settings, user_settings
+from app.service import auth_token
 
 
 api_router = APIRouter()
@@ -10,8 +11,10 @@ api_router.include_router(
     prefix=user_settings.API_USER,
     tags=['users']
 )
+# TODO: 의존성 추가
 api_router.include_router(
     study_rooms.router,
     prefix=study_rooms_settings.API_STUDY_ROOM,
+    # dependencies=[Depends(auth_token),],
     tags=['study_rooms']
-) 
+)
