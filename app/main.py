d9d8f7db0be7111602a5daea9c4b441f946e4d32
2_taehyun <63915557+0417taehyun@users.noreply.github.com>
2021-05-21 00:07:07 +0900

    수정: 소켓 통신 연결 수정

--- app/main.py
+++ app/main.py
@@ -5,10 +5,12 @@ from fastapi     import FastAPI
 
 from app.api     import api_router
 from app.core    import common_settings
-# from app.service import sio
+from app.service import sio
 
 
 server  = FastAPI(title=common_settings.PROJECT_NAME)
+sio_app = socketio.ASGIApp(socketio_server=sio, other_asgi_app=server)
+server.add_websocket_route("/socket.io/", sio_app)
 server.include_router(api_router, prefix=common_settings.COMMON_API)
 
 
--- app/service/__init__.py
+++ app/service/__init__.py
@@ -1,1 +1,2 @@
+from app.service.auth    import auth_token
 from app.service.sockets import sio
