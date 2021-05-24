102e37986e5ad31024dc882dab7a681495d857a8
junkyu.lim <junkyu.lim.dev@gmail.com>
2021-05-25 01:42:10 +0900

    수정 : model 이슈 사항 수정

--- app/models/disturbances.py
+++ app/models/disturbances.py
@@ -11,7 +11,7 @@ from sqlalchemy.orm import relation
 from app.database   import Base
 
 
-class Type(str, enum.Enum):
+class DistType(str, enum.Enum):
     SMARTPHONE = 'smartphone'
     AWAIT      = 'await'
     SLEEP      = 'sleep'
@@ -19,11 +19,11 @@ class Type(str, enum.Enum):
 
 class Disturbances(Base):
     __tablename__ = 'disturbances'
-    id            = Column(Integer(), primary_key=True, autoincrement=True)
-    type          = Column(Enum(Type), nullable=False)
-    count         = Column(Integer(), nullable=False)
-    time          = Column(Integer(), nullable=False)
-    my_study_id   = Column(Integer(), ForeignKey('my_studies.id', ondelete=True))
-    report_id     = Column(Integer(), ForeignKey('reports.id', ondelete=True))
-    my_study      = relation('MyStudies', back_populates='disturbance')
-    report        = relation('Reports', back_populates='total_disturbance')
+    id            = Column('dis_id', Integer, primary_key=True, autoincrement=True)
+    type          = Column('dis_type', Enum(DistType), nullable=False)
+    count         = Column('dis_cnt', Integer, nullable=False)
+    time          = Column('dis_time', Integer, nullable=False)
+    my_study_id   = Column(Integer, ForeignKey('my_studies.id', ondelete='CASCADE'))
+    report_id     = Column(Integer, ForeignKey('reports.id', ondelete='CASCADE'))
+    # my_study      = relation('MyStudies', back_populates='disturbance')
+    # report        = relation('Reports', back_populates='total_disturbance')
