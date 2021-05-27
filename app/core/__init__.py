dafaf7242cc497918f678b0afa038636594a3a3e
2_taehyun <63915557+0417taehyun@users.noreply.github.com>
2021-05-27 22:05:28 +0900

    수정: CORS

--- app/core/__init__.py
+++ app/core/__init__.py
@@ -1,6 +1,7 @@
 from app.core.config import (
                         common_settings,
                         develop_settings,
+                        deploy_settings,
                         study_rooms_settings,
                         user_settings,
                         socket_settings,
