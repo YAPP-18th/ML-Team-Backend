906a6139ea218b3895c099afe80ccd87f2af0b71
limm-jk <junkyu.lim.dev@gmail.com>
2021-07-03 06:09:20 +0900

    추가 : my_study id로 조회하는 api 추가

--- app/api/my_studies.py
+++ app/api/my_studies.py
@@ -60,3 +60,41 @@ def get_my_studies(date: str, user_id: int, db: Session = Depends(get_db)):
     
     finally:
         db.close()
+
+
+@router.get(
+    '/{my_study_id}',
+    responses = {
+        200: {
+            "model": GetMyStudiesResponse,
+            "description": "마이스터디 조회 성공"
+        },
+        500: {
+            "model": ErrorResponseBase,
+            "description": "서버에서 잡지 못한 에러"
+        }
+    }
+)
+def get_my_studies(my_study_id: int, db: Session = Depends(get_db)):
+    try:
+        data = my_studies.get_by_id(db, my_study_id)
+        return JSONResponse(
+            status_code = status.HTTP_200_OK,
+            content     = {'data': data}
+        )
+
+    except NoSuchElementException:
+        return JSONResponse(
+            status_code = status.HTTP_200_OK,
+            content     = {'data': []}
+        )
+    
+    except Exception as error:
+        print(traceback.print_exc())
+        return JSONResponse(
+            status_code = status.HTTP_500_INTERNAL_SERVER_ERROR,
+            content     = {'detail': f'server error: {error}'}
+        )
+    
+    finally:
+        db.close()
