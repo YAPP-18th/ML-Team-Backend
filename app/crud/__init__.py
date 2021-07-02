c7ebdb57e43ff912c43f97e0dbb1e5bc9b61225b
limm-jk <junkyu.lim.dev@gmail.com>
2021-07-03 06:14:31 +0900

    수정 : socket 서버 관련 오류 수정

--- app/crud/__init__.py
+++ app/crud/__init__.py
@@ -1,7 +1,7 @@
-from app.crud.users       import users
-from app.crud.study_rooms import study_rooms
-from app.crud.statuses    import statuses
-from app.crud.my_studies  import my_studies
-from app.crud.reports     import reports
-from app.crud.redis_function       import redis_function
+from app.crud.users          import users
+from app.crud.study_rooms    import study_rooms
+from app.crud.statuses       import statuses
+from app.crud.my_studies     import my_studies
+from app.crud.reports        import reports
+from app.crud.redis_function import redis_function
 
